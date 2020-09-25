65bbe851ec42b20e37fbf20dbd35e4ecaceafc9e
Hitesh Kumar <42475956+megactrl@users.noreply.github.com>
2020-09-26 00:41:06 +0530

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,39 @@
+def hangman(word):
+    wrong = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter"
+        char = input(msg)
+        if char in rletters:
+            v = rletters.index(char)
+            board[v] = char
+            rletters[v]s = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n"
+              .join(stages[0: wrong]))
+        print("You lose! It was {}.".format(word))
+
+hangman("megactrl")
